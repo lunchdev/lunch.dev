08c4483cae0d17e6c36962e966fb44c4f49dfb3c
Michael Chan <mijoch@gmail.com>
2024-12-19 16:52:44 -0800

    npx prettier -w .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import {defineConfig, envField} from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
 	env: {
 		schema: {
-			YOUTUBE_API_KEY: envField.string({ context: "server", access: "secret" })
-		}
-	}
-});
+			YOUTUBE_API_KEY: envField.string({
+				context: 'server',
+				access: 'secret',
+			}),
+		},
+	},
+})
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,35 +1,35 @@
-import { YOUTUBE_API_KEY } from "astro:env/server";
-import { defineCollection, z } from "astro:content";
+import {YOUTUBE_API_KEY} from 'astro:env/server'
+import {defineCollection, z} from 'astro:content'
 
 const youtube_videos = defineCollection({
-  loader: async () => {
-    const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLnc_NxpmOxaM-yvNRB9MrZkLxF9GsB30C&key=${YOUTUBE_API_KEY}`,
-    );
-    const data = await response.json();
-    return data.items;
-  },
-  //  // Must return an array of entries with an id property, or an object with IDs as keys and entries as values
-  //  return data.map((country) => ({
-  //    id: country.cca3,
-  //    ...country,
-  //  }));
-  //},
-  schema: z.object({
-    id: z.string(),
-    snippet: z.object({
-      title: z.string(),
-      description: z.string(),
-      thumbnails: z.object({
-        default: z.object({
-          url: z.string(),
-        }),
-      }),
-    }),
-    contentDetails: z.object({
-      videoId: z.string(),
-    }),
-  }),
-});
+	loader: async () => {
+		const response = await fetch(
+			`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLnc_NxpmOxaM-yvNRB9MrZkLxF9GsB30C&key=${YOUTUBE_API_KEY}`
+		)
+		const data = await response.json()
+		return data.items
+	},
+	//  // Must return an array of entries with an id property, or an object with IDs as keys and entries as values
+	//  return data.map((country) => ({
+	//    id: country.cca3,
+	//    ...country,
+	//  }));
+	//},
+	schema: z.object({
+		id: z.string(),
+		snippet: z.object({
+			title: z.string(),
+			description: z.string(),
+			thumbnails: z.object({
+				default: z.object({
+					url: z.string(),
+				}),
+			}),
+		}),
+		contentDetails: z.object({
+			videoId: z.string(),
+		}),
+	}),
+})
 
-export const collections = { youtube_videos };
+export const collections = {youtube_videos}
