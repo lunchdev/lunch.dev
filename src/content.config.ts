49a90cbe9af7873ab0191f3264a9724e5df405b8
Michael Chan <mijoch@gmail.com>
2024-12-21 17:53:56 -0800

    Create minimal getStaticPaths page, with props for content

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,20 +7,14 @@ const essential_react_16 = defineCollection({
 			`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLnc_NxpmOxaM-yvNRB9MrZkLxF9GsB30C&key=${YOUTUBE_API_KEY}`
 		)
 		const data = await response.json()
-		console.log(data.items)
 		return data.items
 	},
-	//  // Must return an array of entries with an id property, or an object with IDs as keys and entries as values
-	//  return data.map((country) => ({
-	//    id: country.cca3,
-	//    ...country,
-	//  }));
-	//},
 	schema: z.object({
 		id: z.string(),
 		snippet: z.object({
 			title: z.string(),
 			description: z.string(),
+			position: z.number(),
 			thumbnails: z.object({
 				default: z.object({
 					url: z.string(),
