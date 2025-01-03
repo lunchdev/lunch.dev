732a852f064128954d6821b465e6a32f73c6b767
Michael Chan <mijoch@gmail.com>
2025-01-02 22:23:17 -0800

    add tailwind support: npx astro add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
-import {defineConfig, envField} from 'astro/config'
+import { defineConfig, envField } from 'astro/config'
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	env: {
-		schema: {
-			YOUTUBE_API_KEY: envField.string({
-				context: 'server',
-				access: 'secret',
-			}),
-		},
-	},
+    env: {
+        schema: {
+            YOUTUBE_API_KEY: envField.string({
+                context: 'server',
+                access: 'secret',
+            }),
+        },
+    },
+
+    integrations: [tailwind()],
 })
